9e0f02ed488f628bbc88c386f4172c0ab4811db9
printix <gamesmen67@gmail.com>
2025-05-11 02:11:46 +1000

    FINISHED LEVEL
    Fully completed, level design, lighting and what not. Added custom level transition script

---
+++ Assets/Scripts/Level1-Scripts/LevelTransition.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public Animator transitionAnimator;
+    public int nextSceneIndex; 
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            
+            transitionAnimator.SetTrigger("FadeOut");
+
+            Invoke("LoadNextScene", 3f);
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneIndex); 
+    }
+}
