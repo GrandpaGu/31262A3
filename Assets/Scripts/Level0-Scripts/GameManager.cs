aac0ea9eabead7038ff76d431394fc389fdfcdb6
GrandpaGu <151175975+GrandpaGu@users.noreply.github.com>
2025-05-11 17:24:16 +1000

    L4 added
    L4 added

--- Assets/Scripts/Level0-Scripts/GameManager.cs
+++ Assets/Scripts/Level0-Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [Header("玩家对象 (拖拽 Slime)")]
+    [Header("玩家对蟻E(拖拽 Slime)")]
     public SlimeController player;          // 在 Inspector 把 Slime 拖进来
 
     [HideInInspector] public bool isInputLocked = false;
