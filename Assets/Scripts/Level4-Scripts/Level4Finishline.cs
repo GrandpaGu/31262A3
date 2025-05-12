c5cb07d56f0c292da6fe6f250b2700e764c8da12
Chiester556 <joshua.y.kim@student.uts.edu.au>
2025-05-12 19:33:38 +1000

    Level 4 to menu

---
+++ Assets/Scripts/Level4-Scripts/Level4Finishline.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level4Finishline : MonoBehaviour
+{
+    public Animator transitionAnimator;
+    public int nextSceneIndex;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            transitionAnimator.SetTrigger("FadeOut");
+
+            Invoke("LoadNextScene", 1f);
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
